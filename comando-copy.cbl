1bb95a274853d7d9d5fc5405e28473481fdbe7b8
F3952579 Heitor Jr <heitor.junior@bb.com.br>
2024-09-24 00:03:01 -0300

    Fazendo comando-copy compilar

--- comando-copy.cbl
+++ comando-copy.cbl
@@ -5,36 +5,44 @@
       ***   OBJETIVO DO PROGRAMA = PERFORM COM REPETICOES CONTROLADAS
       ***                            POR SAIDA FORCADA (UNTIL)
       ***                            USANDO BOOK
-      ***   AUTOR: IVAN
-      ***   DATA : XX/XX/20XX
+      ***   AUTOR: HEITOR JR
+      ***   DATA : 24/09/2024
       ******************************************************
+
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
            DECIMAL-POINT IS COMMA.
+       
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-           COPY 'BOOK.COB'.
+           COPY 'BOOK.CPY'.
+       
        PROCEDURE DIVISION.
        0001-PRINCIPAL.
-            PERFORM 0100-INICIALIZAR.
-             IF WRK-VENDAS GREATER 0
+           PERFORM 0100-INICIALIZAR
+           IF WRK-VENDAS GREATER 0
                PERFORM 0200-PROCESSAR UNTIL WRK-VENDAS = 0
-            END-IF.
-           PERFORM 0300-FINALIZAR.
-           STOP RUN.
+           END-IF
+           PERFORM 0300-FINALIZAR
+           STOP RUN
+           .
 
        0100-INICIALIZAR.
-            DISPLAY 'DIGITE O NUMERO..... : '.
-            ACCEPT WRK-VENDAS.
+           DISPLAY 'DIGITE O NUMERO..... : '
+           ACCEPT WRK-VENDAS
+           .
 
        0200-PROCESSAR.
-             ADD 1 TO WRK-QT.
-             ADD WRK-VENDAS TO WRK-ACUM.
-              PERFORM 0100-INICIALIZAR.
+           ADD 1 TO WRK-QT
+           ADD WRK-VENDAS TO WRK-ACUM
+           PERFORM 0100-INICIALIZAR
+           .
 
        0300-FINALIZAR.
-             DISPLAY  '---------------------'.
-              DISPLAY 'TOTAL DAS VENDAS     ' WRK-ACUM.
-              DISPLAY 'QUANTIDADE DE VENDAS ' WRK-QT.
-              DISPLAY ' FIM DE PROCESSAMENTO '.
+           DISPLAY  '---------------------'
+           DISPLAY 'TOTAL DAS VENDAS     ' WRK-ACUM
+           DISPLAY 'QUANTIDADE DE VENDAS ' WRK-QT
+           DISPLAY ' FIM DE PROCESSAMENTO '
+           .
+
